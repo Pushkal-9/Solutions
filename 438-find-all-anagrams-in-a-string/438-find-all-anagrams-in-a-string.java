aae4b80c74629aa7b65c67e5b4339f15f22c6538
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2022-01-15 02:40:47 +0530

    Time: 803 ms (10.74%), Space: 141.9 MB (5.44%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -4,13 +4,19 @@ class Solution {
         int m=s.length();
         
         ArrayList<Integer> res=new ArrayList<>();
+        HashMap<Character,Integer> map=new HashMap<>();
         
-        char[]s1=p.toCharArray();
-        Arrays.sort(s1);
-        String k=new String(s1);
+        for(int i=0;i<n;i++){
+            if(!map.containsKey(p.charAt(i))){
+                map.put(p.charAt(i),1);
+            }
+            else{
+                map.put(p.charAt(i),map.get(p.charAt(i))+1);
+            }
+        }
         
         for(int i=0;i<m-n+1;i++){
-            if(isAnagram(s.substring(i,i+n),k)){
+            if(isAnagram(s.substring(i,i+n),p)){
                 res.add(i);
                 while(i+n<m && s.charAt(i)==s.charAt(i+n)){
                     res.add(++i);
@@ -21,14 +27,52 @@ class Solution {
         return res;
     }
     
-    public boolean isAnagram(String a,String t){
-        char[]s1=a.toCharArray();
-        Arrays.sort(s1);
-        String k=new String(s1);
-        if(k.equals(t)){
-            return true;
-        }
+//     public boolean isAnagram(String a,HashMap<Character,Integer> mapping){
+// //         char[]s1=a.toCharArray();
+// //         Arrays.sort(s1);
+// //         String k=new String(s1);
+// //         if(k.equals(t)){
+// //             return true;
+// //         }
         
-        return false;
+// //         return false;
+        
+        
+//         HashMap<Character,Integer> map=new HashMap<>();
+        
+//         for(int i=0;i<a.length();i++){
+//             if(!map.containsKey(a.charAt(i))){
+//                 map.put(a.charAt(i),1);
+//             }
+//             else{
+//                 map.put(a.charAt(i),map.get(a.charAt(i))+1);
+//             }
+//         }
+        
+//         for(Character key :map.keySet()){
+//             if(map.get(key)!=mapping.get(key)){
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+    
+    private boolean isAnagram(String s, String p) {
+        int[] count = new int[26];
+        char[] c1 = s.toCharArray();
+        char[] c2 = p.toCharArray();
+        
+        for(int i = 0; i < c1.length;i++) {
+            count[c1[i] - 'a']++;
+            count[c2[i] - 'a']--;
+        }
+        //System.out.println(Arrays.toString(count));
+        for(int i = 0;i < count.length;i++) {
+            if(count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
