19ee7d33045a5ee31287ca9c5371c3addbfd6d79
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2022-01-16 03:03:05 +0530

    Time: 3 ms (75.77%), Space: 96.3 MB (81.26%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,19 +1,19 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n=nums.length;
-        for (int i = 0; i < n; i++) {
-        if (nums[i] <= 0 || nums[i] > n) {
-            nums[i] = n + 1;
+        for (int i = 0; i < nums.length; i++) {
+        if (nums[i] <= 0 || nums[i] > nums.length) {
+            nums[i] = nums.length + 1;
         }
     }
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.length; i++) {
         int num = Math.abs(nums[i]);
-        if (num > n) {
+        if (num > nums.length) {
             continue;
         }
-        num--; // -1 for zero index based array (so the number 1 will be at pos 0)
-        if (nums[num] > 0) { // prevents double negative operations
+        num--;
+        if (nums[num] > 0) { 
             nums[num] = -1 * nums[num];
         }
     }
@@ -23,7 +23,7 @@ class Solution {
                 return i+1;
             }
         }
-        return n+1;
+        return nums.length+1;
         
 //         HashSet<Integer> set=new HashSet<>();
         
