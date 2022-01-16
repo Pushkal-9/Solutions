0e9077b634234a7f998f9420e37089cbb1e97ddc
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2022-01-16 20:31:04 +0530

    Time: 3 ms (64.84%), Space: 54.7 MB (27.43%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -2,32 +2,37 @@ class Solution {
     public boolean canReach(int[] arr, int start) {
         int n=arr.length;
         boolean[]vis=new boolean[n];
-        dfs(arr,vis,start,n);
+        boolean res=dfs(arr,vis,start,n);
         
-        for(int i=0;i<n;i++){
-            if(arr[i]==0 && vis[i]){
-                return true;
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==0 && vis[i]){
+        //         return true;
+        //     }
+        // }
         
         
-        return false;
+        return res;
     }
     
-    public void dfs(int[]arr,boolean[]vis,int i,int n){
-        if(i>=n){
-            return;
+    public boolean dfs(int[]arr,boolean[]vis,int i,int n){
+        
+        if(arr[i]==0){
+            return true;
         }
         
         vis[i]=true;
         
         int[]row={arr[i],-arr[i]};
         
+        boolean res=false;
+        
         for(int k=0;k<2;k++){
             if(isSafe(i+row[k],vis,n)){
-                dfs(arr,vis,i+row[k],n);
+                res=res||dfs(arr,vis,i+row[k],n);
             }
         }
+        
+        return res;
     }
     
     public boolean isSafe(int r,boolean[]vis,int n){
