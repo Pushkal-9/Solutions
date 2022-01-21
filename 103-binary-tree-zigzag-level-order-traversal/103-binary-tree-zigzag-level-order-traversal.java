6d3812e7bf9dda09c0a7fcd08e20cb139720bd34
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2022-01-22 01:07:29 +0530

    Time: 1 ms (83.81%), Space: 39.4 MB (41.64%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<List<Integer>> list=new ArrayList<>();
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        bfs(root,height(root));
+        return list;
+    }
+    
+    public void bfs(TreeNode root,int h){
+        
+        for(int i=1;i<=h;i++){
+           ArrayList<Integer>li=new ArrayList<>(); 
+            if(i%2==1){
+                evenLevelOrder(root,i,li);
+            }
+            else{
+                oddLevelOrder(root,i,li);
+            }
+            list.add(li);
+        }
+    }
+    
+    
+    public void evenLevelOrder(TreeNode node,int l,List<Integer> list){
+        if(node==null){
+            return;
+        }
+        if(l==1){
+            list.add(node.val);
+        }
+        if(l>1){
+            evenLevelOrder(node.left,l-1,list);
+            evenLevelOrder(node.right,l-1,list);
+        }
+    }
+    
+    public void oddLevelOrder(TreeNode node,int l,List<Integer> list){
+        if(node==null){
+            return;
+        }
+        if(l==1){
+            list.add(node.val);
+        }
+        if(l>1){
+            oddLevelOrder(node.right,l-1,list);
+            oddLevelOrder(node.left,l-1,list);
+        }
+    }
+    
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        
+        int lh=height(root.left);
+        int rh=height(root.right);
+        
+        return 1+ Math.max(lh,rh);
+    }
+}
