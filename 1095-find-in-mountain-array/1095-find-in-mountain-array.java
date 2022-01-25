013121981f202e3548215ac7b0073f25909dfe62
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2022-01-25 20:26:30 +0530

    Time: 1 ms (25.17%), Space: 39.1 MB (58.21%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,87 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n=mountainArr.length()-1;
+        int peak= peak(mountainArr,0,n);
+        if(mountainArr.get(peak)==target){
+            return peak;
+        }
+        
+        int i=decSearch(mountainArr,0,peak-1,target);
+        int j=incSearch(mountainArr,peak+1,n,target);
+        
+        if(i!=-1){
+            return i;
+        }
+        
+        //System.out.println(i);
+        
+        return j;
+        
+    }
+    
+    public int decSearch(MountainArray arr,int low,int high,int target){
+        int mid=(high-low)/2 +low;
+        if(low>high){
+            return -1;
+        }
+        int k=arr.get(mid);
+        if(k==target){
+            System.out.println(mid);
+            return mid;
+        }
+        
+        else if(k>target){
+            return decSearch(arr,low,mid-1,target);
+        }
+        
+        return decSearch(arr,mid+1,high,target);
+    }
+    
+    public int incSearch(MountainArray arr,int low,int high,int target){
+        int mid=(high-low)/2 +low;
+        if(low>high){
+            return -1;
+        }
+        int k=arr.get(mid);
+        
+        if(k==target){
+            return mid;
+        }
+        
+        else if(k<target){
+            return incSearch(arr,low,mid-1,target);
+        }
+        
+        return incSearch(arr,mid+1,high,target);
+    }
+    
+    public int peak(MountainArray arr,int low,int high){
+        int mid=0;
+        while(low<high){
+        mid=(high-low)/2 +low;
+        int k=arr.get(mid);
+        // int prev=arr.get(mid-1);
+        // int next=arr.get(mid+1);
+        if(mid-1>=0 && mid+1<arr.length() && k>arr.get(mid-1) && k>arr.get(mid+1)){
+            return mid;
+        }
+        else if(mid-1>=0 && mid+1<arr.length() && arr.get(mid-1)<k && k<arr.get(mid+1)){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+        }
+        
+        return low;
+    }
+}
