118de573d7a8fc5fd03924c33fa31a4728d739c9
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2023-11-02 16:37:43 -0400

    Time: 60 ms (60.09%), Space: 55.3 MB (65.32%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,18 +5,12 @@ class Solution {
         Arrays.fill(res,0);
         for(int i=0;i<temperatures.length;i++){
             int val = temperatures[i];
-            
-            if(stack.isEmpty()){
-                stack.push(i);
-            }
-            else{
                 while(!stack.isEmpty() && temperatures[stack.peek()]< val){
                     int index= stack.pop();
                     res[index]=i-index;
                 }
                 
                 stack.push(i);
-            }
         }
         
         return res;
