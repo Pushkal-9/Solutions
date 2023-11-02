78793e710a7887290e45e935908aee841263f9d8
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2023-11-02 14:17:06 -0400

    Time: 3 ms (12.32%), Space: 40.5 MB (51.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -7,14 +7,12 @@ class Solution {
         while(i<s.length()){
             if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
                 stack.push(s.charAt(i));
-                System.out.println(s.charAt(i));
             }
             else{
                 if(stack.size()==0){
                     return false;
                 }
                 char ch = stack.pop();
-                                System.out.println(ch);
                 if((ch=='{' && s.charAt(i)=='}')){
                                 i++;
 
