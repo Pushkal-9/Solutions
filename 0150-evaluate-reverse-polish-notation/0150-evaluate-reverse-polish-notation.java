21f3641aca153bf97b2f344372767486c0ac2710
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2023-11-02 16:05:55 -0400

    Time: 6 ms (78.66%), Space: 43.2 MB (55.39%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -5,24 +5,26 @@ class Solution {
         for(int i=0;i<tokens.length;i++){
             String op = tokens[i];
             
-            if(op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/")){
-                Integer second = stack.pop();
-                //System.out.println(second +" " + stack.size());
-                Integer first = stack.pop();
-                //System.out.println(first);
                 
                 if(op.equals("+")){
+                                    Integer second = stack.pop();
+                Integer first = stack.pop();
                     stack.push(first+second);
                 }
                 else if (op.equals("-")){
+                                    Integer second = stack.pop();
+                Integer first = stack.pop();
                     stack.push(first-second);
                 }
                 else if(op.equals("*")){
+                                    Integer second = stack.pop();
+                Integer first = stack.pop();
                     stack.push(first*second);
                 }
                 else if(op.equals("/")){
+                                    Integer second = stack.pop();
+                Integer first = stack.pop();
                     stack.push(first/second);
-                }
             }
             else{
                 Integer k = Integer.parseInt(op);
