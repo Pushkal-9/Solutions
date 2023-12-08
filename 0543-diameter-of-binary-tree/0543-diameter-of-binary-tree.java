1c26a3fdeb3cffacb2180be48bcaaa1d988d5ed4
Pushkal-9 <61682783+Pushkal-9@users.noreply.github.com>
2023-12-07 23:53:34 -0500

    Time: 6 ms (16.21%), Space: 42.1 MB (24.76%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        
+        int current = height(root.left) + height(root.right);
+        
+        return Math.max(current, Math.max(diameterOfBinaryTree(root.left),diameterOfBinaryTree(root.right)));
+    }
+    
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        
+        int left = height(root.left);
+        int right = height(root.right);
+        
+        return 1+Math.max(left,right);
+    }
+}
